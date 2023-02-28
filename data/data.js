58032be9ba93ee4853ac7bf02fc85670b66b1097
Kanayonickel <aloysiuspendergast@protonmail.com>
2023-02-28 05:46:59 -0800

    first commit

---
+++ data/data.js
@@ -0,0 +1,91 @@
+export const data = [
+    {
+        id: 1,
+        name: {
+            first:'Kanayo',
+            last: 'Maduka',
+        },
+        total: 6856,
+        status: 'On Hold',
+        method: 'PayDay',
+        email: 'vacay@gmail.com',
+        date: '10 Minutes ago',
+    },
+    {
+        id:2,
+        name: {
+            first: 'Genevieve',
+            last: 'Okoye',
+         
+        },
+
+        total: 5632,
+        status: 'Processing',
+        method: 'Paystack',
+        date: '12 Minutes ago',
+    },
+    {
+        id: 3,
+        name: {
+            first: 'Omotola',
+            last: 'Ekeinde',
+        },
+        total: 1275,
+        status: 'Processing',
+        method: 'Flutterwave',
+        email: 'aiko@gmail.com',
+        date: '24 minutes ago',
+    },
+    {
+        id: 3,
+        name: {
+            first: 'Sarah',
+            last: 'Bosque',
+        },
+        total: 3245,
+        status: 'Completed',
+        method: 'Flutterwave',
+        email: 'gfw@gmail.com',
+        date: '24 minutes ago',
+
+    }
+    ,
+    {
+        id: 4,
+        name: {
+            first: 'Isaac',
+            last: 'Newton',
+        },
+        total: 3000,
+        status: 'Processing',
+        method: 'Flutterwave',
+        date: '29 minutes ago',
+
+    },
+    {
+        id: 5,
+        name: {
+            first: 'Neil',
+            last: 'Bohr',
+        },
+        total: 31245,
+        status: 'Completed',
+        method: 'Flutterwave',
+        email: 'jui@gmail.com',
+        date: '1 hour ago',
+
+    },
+    {
+        id: 6,
+        name: {
+            first: 'Beyonce',
+            last: 'Knowles',
+        },
+        total: 3245,
+        status: 'Processing',
+        method: 'PayDay',
+        email: 'ghi@gmail.com',
+        date: 'I hour ago',
+
+    }
+]
